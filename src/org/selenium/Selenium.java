0492a4dbef8debc1e2a2695a3a3b15ed4219e7e0
SyedIrfanali03 <greenstaskmay@gmail.com>
2024-04-16 11:31:24 +0530

    raja added

--- src/org/selenium/Selenium.java
+++ src/org/selenium/Selenium.java
@@ -10,6 +10,9 @@ public class Selenium {
 	public static void main(String[] args) {
 		
 	System.out.println("We are Doing seleniu projects ");	
+	
+	
+	System.out.println("Raja code added");
 	}
 	
 
